00f5829f169871ecc0f8b20f8844533a7fe87534
Jovan <126861699+Jayvan123@users.noreply.github.com>
2025-11-15 11:36:52 +0800

    Your commit message

---
+++ src/components/ProctorMonitoring.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProctorMonitoring = () => {
+  return (
+    <View>
+      <Text>This part is for the Procto monitoring</Text>
+      <Text>Only the Scheduler can view this part of the system he/she can only view the list of all exam scheduler of the proctor really inputed his/her code</Text>
+      <Text>But im not sure how appraoch i will do in this part like the UI part this is can you suggest? </Text>
+    </View>
+  )
+}
+
+export default ProctorMonitoring
+
+const styles = StyleSheet.create({})
